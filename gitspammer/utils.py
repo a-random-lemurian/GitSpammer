53e07a29506c04e6611e23dc873e01c3d2d78f94
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2021-11-14 21:47:44 +0800

    Actually use length arg in get_letters

--- gitspammer/utils.py
+++ gitspammer/utils.py
@@ -3,4 +3,4 @@ import random
 LETTERS = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 def get_letters(length: int):
-    return ''.join(random.choices(LETTERS,k=12))
+    return ''.join(random.choices(LETTERS,k=length))
